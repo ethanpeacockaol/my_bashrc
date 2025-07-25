56d72a3ff71783a1382c9a6d768882b82a7cf9d8
xthan <ethanpeacock@aol.com>
2025-07-25 13:19:52 -0400

    hey this should work

--- git_sync/gitsync.py
+++ git_sync/gitsync.py
@@ -1,19 +1,25 @@
 import os
 import subprocess
-# add the todo directory
+# hey comment me out when you install the gitsync command into aliases (.bashrc) and place gitsync.py in the github dir named .gitsync
 print('hey add gitsync to Github directory as .gitsync.py right and then add an alias to call it from anywhere')
 dirs_to_pull = ["/data/data/com.termux/files/home/x/TODO"]
-
-
-# deviations... ubuntu does not have an x directory...
-# fixes include uhhh changing the tool when you download it to point to the correct todo directory
-# or add x directory to ubuntu
-# i think i'll just change the path right
-
-# Here is the command that returns a list of strings that represent an ls output
-
+prepend = "/data/data/com.termux/files/home/x/Github/"
+status = ["TODO"]
 
 ls_output = subprocess.run("ls", capture_output=True, text=True)
 ls_items = ls_output.stdout.split("\n")[:-1]
+status += ls_items
+
 for item in ls_items:
-    print(item)
+    dirs_to_pull += [f"{prepend}{item}"]
+
+print("________________Git Sync_______________________")
+i = 0
+for dirx in dirs_to_pull:
+    print(f"DIRECTORY: {status[i]}")
+    os.system(f"cd {dirx} && git pull")
+    print("\n\n")
+    i += 1
+
+print("\n\nGit Sync Done.")
+print("_______________________________________________")
